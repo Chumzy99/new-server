f81f2f7f396e16e240615f84993d7043f9a56fa3
nwagu chima <chima.nwagu@decagon.dev>
2021-11-15 09:01:54 +0100

    initial commit

--- server/server/app.ts
+++ server/server/app.ts
@@ -3,11 +3,14 @@ import http, { IncomingMessage, Server, ServerResponse } from "http";
 implement your server code here
 */
 
-const server :Server = http.createServer((req: IncomingMessage, res: ServerResponse) => {
+const server: Server = http.createServer(
+  (req: IncomingMessage, res: ServerResponse) => {
     if (req.method === "GET") {
       res.end(JSON.stringify({ name: "hello" }));
     }
   }
 );
 
-server.listen(3005);
+server.listen(3005, "127.0.0.1", () => {
+  console.log("Listening to request on port 3005...");
+});
